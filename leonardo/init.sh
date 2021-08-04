325983f11f7b4cdd31eac9c47051c207df861f65
Qi Wang <qi@broadinstitute.org>
2021-08-04 17:55:53 -0400

    update

--- leonardo/init.sh
+++ leonardo/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-pip install terra-notebook-utils
+# pip install terra-notebook-utils
 
 # Assume the managed identity to get proper permission
 az login --identity
 
 # Download certs from azure storage
+jupyter lab  --autoreload --NotebookApp.token='' --NotebookApp.password='' 
